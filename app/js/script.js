23261ec602896dfa8bab8034f1562eedf93ad845
Øystein Håberg <islandstone89@gmail.com>
2024-09-28 23:20:01 +0200

    * Added js for form validation

---
+++ app/js/script.js
@@ -0,0 +1,43 @@
+
+/* SELECTORS */
+
+let form = document.querySelector(".form");
+let emailInput = document.querySelector(".email-input");
+let errorValue = document.querySelector(".form__error__no-value");
+let errorEmail = document.querySelector(".form__error__no-email");
+let successMessage = document.querySelector(".form__success");
+let emailSymbol = "@";
+
+/* FUNCTIONS */
+
+function checkValue(e) {
+    e.preventDefault();
+    if (emailInput.value === "") {
+        errorValue.style.display = "block"
+        successMessage.style.display = "none"
+    }
+    else if (!emailInput.value.includes(emailSymbol)) {
+        errorEmail.style.display = "block"
+        errorValue.style.display = "none"
+        successMessage.style.display = "none"
+    }
+    else {
+        errorValue.style.display = "none"
+        errorEmail.style.display = "none"
+        successMessage.style.display = "block"
+    }
+}
+
+ /*    function test(e) {
+        e.preventDefault();
+
+        if (emailInput.value === "") {
+            emailInput.setAttribute("aria-invalid", "true")
+        }
+    } */
+
+/* EVENT LISTENER */
+
+form.addEventListener("submit", checkValue);
+
+/* form.addEventListener("submit", test); */
